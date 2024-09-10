57825540bf6978eda32478a0e6ab3c0f9093b089
anildalar <dollorinfotech@gmail.com>
2024-09-10 09:19:48 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -32,18 +32,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //1. Property
   //             VariableTYpe _ceo          = ClassName();
-  final TextEditingController _textController = TextEditingController();
+  final TextEditingController _usernametextController = TextEditingController();
+  final TextEditingController _passtextController = TextEditingController();
+  final TextEditingController _cpasstextController = TextEditingController();
+
+  //2. Constructor
+
+  //3. Method
 
   void _handleSubmit() async {
-    var inputText = _textController.text;
+    var userinputText = _usernametextController.text;
+    var passinputText = _passtextController.text;
+    var cpassinputText = _cpasstextController.text;
     // Perform any action with the input (e.g., print it or update the UI)
-    print('Input Text: $inputText');
+    print('Input Text: $userinputText');
+    print('Input Text: $passinputText');
+    print('Input Text: $cpassinputText');
 
-    var url = Uri.parse('http://localhost:1337/api/best-friends');
+    var url = Uri.parse('http://localhost:1337/api/auth/local/register');
     //Let preparet the payload
     var payload = jsonEncode({
-      "data": {"name": inputText}
+      "username": userinputText,
+      "email": passinputText,
+      "password": cpassinputText
     });
     try {
       // Make the POST request
@@ -54,9 +67,27 @@ class _MyHomePageState extends State<MyHomePage> {
           body: payload);
       // Check if the request was successful
       if (response.statusCode == 200 || response.statusCode == 201) {
-        print('Successfully added: $inputText');
+        //Show alert for
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text("User Registered Successfully"),
+              content: Text("User Registered Successfully"),
+              actions: <Widget>[
+                TextButton(
+                  child: Text("OK"),
+                  onPressed: () {
+                    Navigator.of(context).pop(); // Close the dialog
+                  },
+                ),
+              ],
+            );
+          },
+        );
+        print('User registered Successfully');
       } else {
-        print('Failed to add friend. Status code: ${response.statusCode}');
+        print('User registration Failed');
       }
     } catch (e) {
       print('Error occurred: $e');
@@ -78,9 +109,23 @@ class _MyHomePageState extends State<MyHomePage> {
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           TextField(
-            controller: _textController,
+            controller: _usernametextController,
             decoration: const InputDecoration(
-              labelText: 'Enter your input',
+              labelText: 'Enter your Username',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          TextField(
+            controller: _passtextController,
+            decoration: const InputDecoration(
+              labelText: 'Enter your password',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          TextField(
+            controller: _cpasstextController,
+            decoration: const InputDecoration(
+              labelText: 'Enter your confirm password',
               border: OutlineInputBorder(),
             ),
           ),
